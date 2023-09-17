297022acf5c8aecb1690308331850ed7d836f7af
Juan José Benito Mateos <jjosebenitom@gmail.com>
2023-09-17 17:46:54 +0200

    Part 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,27 +4,33 @@ import { Content } from './components/Content';
 import { Total } from './components/Total';
 
 export const App = () => {
-	const course = 'Half Stack application development';
-	const parts = [
-		{
-			name: 'Fundamentals of React',
-			exercises: 10,
-		},
-		{
-			name: 'Using props to pass data',
-			exercises: 7,
-		},
-		{
-			name: 'State of a component',
-			exercises: 14,
-		},
-	];
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10,
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7,
+			},
+			{
+				name: 'State of a component',
+				exercises: 14,
+			},
+		],
+	};
 	return (
 		<div>
-			<Header course={course} />
-			<Content contents={parts} />
+			<Header course={course.name} />
+			<Content contents={course.parts} />
 			<Total
-				total={parts[0].exercises + parts[1].exercises + parts[2].exercises}
+				total={
+					course.parts[0].exercises +
+					course.parts[1].exercises +
+					course.parts[2].exercises
+				}
 			/>
 		</div>
 	);
